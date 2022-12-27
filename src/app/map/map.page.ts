825f13700d54f632df497b47cfff4d87c12d78c4
Your Name <decierdo.dexter@dnsc.edu.ph>
2022-12-27 18:36:56 +0800

    2nd commit

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-//import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/ngx';
+import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/ngx';
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
@@ -8,61 +8,68 @@ import * as L from 'leaflet';
 })
 export class MapPage implements OnInit{
   map!: L.Map;
+  AndroidPermissions = new AndroidPermissions;
+  
 
   ngOnInit(){
     //Accessing Device Location
-      // if(!navigator.geolocation){
-      //   console.log('location not supported')
-      // }
-      // navigator.geolocation.getCurrentPosition((position) => {
-      //   let coord = position.coords;
+    this.permit();
+      if(!navigator.geolocation){
+        console.log('location not supported')
+      }
+      navigator.geolocation.getCurrentPosition((position) => {
+        let coord = position.coords;
       //   //Accessing Device Location - END
 
         //Integrate leaflet
     //coor.longitude&coor.latitude to get device location
     // since there's an issue with the android permission we can't use gps
-        this.map = L.map('mapId').setView([50.5,-15], 5); 
+        this.map = L.map('mapId').setView([coord.latitude, coord.longitude], 5); 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-          maxZoom: 10,
-          minZoom: 5,
+          maxZoom: 20,
+          minZoom: 2,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">MOpenStreetap</a> contributors'
         }).addTo(this.map),{
              
              
              };
             
-        //Marker   
-        // L.marker([coord.latitude, coord.longitude]).addTo(this.map)
-        // .bindPopup("You're Here")
-        // .openPopup();
-      // });
+        // Marker   
+        L.marker([coord.latitude, coord.longitude]).addTo(this.map)
+        .bindPopup("You're Here")
+        .openPopup();
+      });
 
 
-      // this.watchPosition();
+      this.watchPosition();
   }
-  // watchPosition(){
+  watchPosition(){
   
-  //   let id = navigator.geolocation.watchPosition((position) =>{
-  //     console.log(`lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`);
+    let id = navigator.geolocation.watchPosition((position) =>{
+      console.log(`lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`);
  
-  //   },(err) => {
-  //     console.log(err);
-  //   },{
-  //     enableHighAccuracy: true,
-  //     maximumAge: 0,
-  //     timeout: 5000
-  //   })
+    },(err) => {
+      console.log(err);
+    },{
+      enableHighAccuracy: true,
+      maximumAge: 0,
+      timeout: 5000
+    })
   }
 
-  // permit(){
-  //   this.permissions.checkPermission(this.permissions.PERMISSION.ACCESS_FINE_LOCATION).then(
-  //          result => {
-  //           if(!result.hasPermission){
+  permit(){
+    // To request permission : Edit AndroidManifest.xml and add   
+    // <uses-permission android:name="android.permission.ACCESS_COURSE_LOCATION"/>
+   // <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
+    this.AndroidPermissions.checkPermission(this.AndroidPermissions.PERMISSION.ACCESS_FINE_LOCATION).then(
+           result => {
+            if(!result.hasPermission){
               
-  //             console.log('nopermit'),
-  //             this.permissions.requestPermission(this.permissions.PERMISSION.ACCESS_FINE_LOCATION);
-  //           }
-  //          },
-  //          err => this.permissions.requestPermission(this.permissions.PERMISSION.ACCESS_FINE_LOCATION)
-  //        );
-  // }
+              console.log('nopermit'),
+              this.AndroidPermissions.requestPermission(this.AndroidPermissions.PERMISSION.ACCESS_FINE_LOCATION);
+            }
+           },
+           err => this.AndroidPermissions.requestPermission(this.AndroidPermissions.PERMISSION.ACCESS_FINE_LOCATION)
+         );
+  }
+}
